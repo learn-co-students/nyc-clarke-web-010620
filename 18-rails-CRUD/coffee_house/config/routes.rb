946e9a3a06425b459bbb5c988bb9c9d35a6843df
Dwire <gjdwyer8@gmail.com>
2020-01-31 16:43:14 -0500

    CR

---
+++ 18-rails-CRUD/coffee_house/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :drinks, only: [:index, :show, :new, :create]
+
+  # get 'drinks', to: 'drinks#index', as: "drinks"
+  # get 'drinks/new', to: 'drinks#new', as: "drink"
+  # get 'drinks/:id', to: 'drinks#show', as: "new_drink"
+
+  # post '/drinks', to 'drinks#create'
+end
